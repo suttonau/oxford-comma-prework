f95562d87eea6860906d2d205cdb7a94569c81cf
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-06 14:48:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   else 
-    string = array.join(", ")
-    string << 
+    array[-1] 
+    array.join(", ")
+    
     
 end
